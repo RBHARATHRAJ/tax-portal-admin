407a5b92d65d332109197861918aa69dbe2a18e0
piyushmagure <piyush.sharma@delphic.in>
2026-02-09 13:35:26 +0530

    layout fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'http://localhost:8080/api/v1';
 
 export const usersAPI = {
   getAll: () => axios.get(`${API_BASE_URL}/users`),
